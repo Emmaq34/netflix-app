ed2be9a6a5fc2d2e90c91a98700db61bf30c8c40
xiaochenxc <xiaocheq@gmail.com>
2019-07-28 12:33:26 -0700

    Updates

---
+++ precache-manifest.106595fe173003773014c800a0ca70db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8b968ddd4d07810cbdc8df94e5afed",
+    "url": "/netflix-app/index.html"
+  },
+  {
+    "revision": "f1580c62c504ac63dd39",
+    "url": "/netflix-app/static/css/main.df3d4728.chunk.css"
+  },
+  {
+    "revision": "8367aca1fc4c54ff365a",
+    "url": "/netflix-app/static/js/2.eee75552.chunk.js"
+  },
+  {
+    "revision": "f1580c62c504ac63dd39",
+    "url": "/netflix-app/static/js/main.696e8d8d.chunk.js"
+  },
+  {
+    "revision": "018b0c5034e0d1774373",
+    "url": "/netflix-app/static/js/runtime~main.3b6ece1f.js"
+  },
+  {
+    "revision": "002d22505ecac8ad22a8bec767627371",
+    "url": "/netflix-app/static/media/Netflix-Logo.002d2250.png"
+  }
+]);
